7e840a8fca5b1ec13b73ab290c4496a845e1c5c7
Martin Fuhrer <martin@fuhrer.com>
2021-05-22 10:14:08 +0000

    week 1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,10 +8,8 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
-    # path for about view
-
-    # path for contact us view
+    path(route='about', view=views.about, name='about'),
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
 
